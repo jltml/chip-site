7b0bbd61179d4a64919b6ad245512a65b6240df9
jltml <8261330+jltml@users.noreply.github.com>
2023-02-10 14:50:40 -0500

    First commit!

---
+++ frontend/javascript/index.js
@@ -0,0 +1,10 @@
+import "index.css"
+import "syntax-highlighting.css"
+
+// Import all JavaScript & CSS files from src/_components
+import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
+
+// import ScrollReveal from 'scrollreveal'
+// ScrollReveal().reveal('.reveal')
+
+console.info("Bridgetown is loaded!")
---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: {
+    'postcss-import': {},
+    'tailwindcss': {},
+    'postcss-flexbugs-fixes': {},
+    'postcss-preset-env': {
+      autoprefixer: {
+        flexbox: 'no-2009'
+      },
+      stage: 2
+    }
+  }
+}
